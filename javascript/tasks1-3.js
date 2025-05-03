d96d9881a6dc52b5e86c02093755fb2eccb71fc4
Steven Trumble <strumble91@gmail.com>
2025-05-03 14:48:41 +1000

    tasks 1-3

---
+++ javascript/tasks1-3.js
@@ -0,0 +1,15 @@
+let Name = "Steven";
+console.log(Name);
+let age = 33;
+console.log(age);
+let isStudent = true;
+console.log(isStudent);
+console.log(Name, age, isStudent);
+
+let dividedByZero = 5/0;
+console.log(dividedByZero, typeof dividedByzero);
+let notANumber = 5 * "4";
+console.log(notANumber, typeof notANumber);
+
+let message = "Hi, I'm Steven and I love js!";
+console.log(message);
