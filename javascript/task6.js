71fbb8b97ddf51dbfb88ba4ba2912cde492e5de4
Steven Trumble <strumble91@gmail.com>
2025-05-05 17:14:25 +1000

    show number repeated that many times

---
+++ javascript/task6.js
@@ -0,0 +1,12 @@
+for(let i = 1; i < 6; i++){
+    console.log(i.toString().repeat(i));
+}
+/*
+Write a loop that prints the following pattern. Each line shows the same number repeated that many times. Use a single loop.
+
+1
+22
+333
+4444
+55555
+*/
