a5eb91b991809a20107e4a8ad60db24ef7e5a0c0
Steven Trumble <strumble91@gmail.com>
2025-05-20 17:48:13 +1000

    foreach and chaning examples

--- week4/forEach.js
+++ week4/forEach.js
@@ -1,3 +1,11 @@
 let names = ['Ash', 'Steven', 'Igor'];
 
 names.forEach(name => console.log(`Hello, ${name}`));
+names.forEach(name => console.log('Hello, ' + name.toUpperCase()));
+
+let numbers = [12, 34, 65];
+let sum = 0;
+numbers.forEach(num => sum += num);
+console.log(sum);
+
+
