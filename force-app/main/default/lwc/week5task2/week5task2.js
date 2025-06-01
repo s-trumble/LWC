7a7d928414d7d9c3d83e829e5b400e04b484cfdb
Steven Trumble <strumble91@gmail.com>
2025-06-01 16:06:46 +1000

    task2 table

--- force-app/main/default/lwc/week5task2/week5task2.js
+++ force-app/main/default/lwc/week5task2/week5task2.js
@@ -1,50 +1,67 @@
 import { LightningElement } from 'lwc';
 
 export default class Week5task2 extends LightningElement {
-    currentMonth = Text(Date.today().Month());
-    currentYear = Text(Date.today().Year());
-    
-    expense1 = {
-        name: 'Grocery Groceries', 
-        category: 'Food', 
-        amount: 700, 
-        date: '15 ' + this.currentMonth + ' ' + this.currentYear
-    };
-    expense2 = {
-        name: 'Petrol', 
-        category: 'Transportation', 
-        amount: 100, 
-        date: '12 ' + this.currentMonth + ' ' + this.currentYear
-    };
+    currentMonth;
+    currentYear;
+    expense1;
+    expense2;
+    expense3;
+    expense4;
+    expense5;
+    expense6;
+    expense7;
 
-    expense3 = {
-        name: 'Mortgage', 
-        category: 'House', 
-        amount: 2000, 
-        date: '21 ' + this.currentMonth + ' ' + this.currentYearexport
-    };
-    expense4 = {
-        name: 'Water', 
-        category: 'House', 
-        amount: 75, 
-        date: '19 ' + this.currentMonth + ' ' + this.currentYear
-    };
-    expense5 = {
-        name: 'Eating Out', 
-        category: 'Food', 
-        amount: 45, 
-        date: '18 ' + this.currentMonth + ' ' + this.currentYear
-    };
-    expense6 = {
+    constructor() {
+        super();
+        const monthNames = [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+        const today = new Date();
+        this.currentMonth = monthNames[today.getMonth()];
+        this.currentYear = today.getFullYear();
+
+        this.expense1 = {
+            name: 'Groceries', 
+            category: 'Food', 
+            amount: 700, 
+            date: '15 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense2 = {
+            name: 'Petrol', 
+            category: 'Transportation', 
+            amount: 100, 
+            date: '12 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense3 = {
+            name: 'Mortgage', 
+            category: 'House', 
+            amount: 2000, 
+            date: '21 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense4 = {
+            name: 'Water', 
+            category: 'House', 
+            amount: 75, 
+            date: '19 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense5 = {
+            name: 'Eating Out', 
+            category: 'Food', 
+            amount: 45, 
+            date: '18 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense6 = {
             name: 'Internet', 
-        category: 'Entertainment', 
-        amount: 100, 
-        date: '12 ' + this.currentMonth + ' ' + this.currentYear
-    };
-    expense7 = {
-        name: 'Streaming', 
-        category: 'Entertainment', 
-        amount: 30,
-        date: '12 ' + this.currentMonth + ' ' + this.currentYear
+            category: 'Entertainment', 
+            amount: 100, 
+            date: '12 ' + this.currentMonth + ' ' + this.currentYear
+        };
+        this.expense7 = {
+            name: 'Streaming', 
+            category: 'Entertainment', 
+            amount: 30,
+            date: '12 ' + this.currentMonth + ' ' + this.currentYear
+        };
     }
 }
