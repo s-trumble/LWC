8269ff3de20ddaf5074c71b5189cb60e9ede1a0e
Steven Trumble <strumble91@gmail.com>
2025-06-01 16:16:49 +1000

    grid solved

--- force-app/main/default/lwc/week5task3/week5task3.js
+++ force-app/main/default/lwc/week5task3/week5task3.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class Week5task3 extends LightningElement {}
+export default class Week5task3 extends LightningElement {
+    leads = {
+        title: 'New Leads',
+        value: 245,
+        subttitle: 'This month'
+    }
+    deals = {
+        title: 'Closed Deals',
+        value: 78,
+        subttitle: 'Q2 Total'
+    }
+    pipeline = {
+        title: 'Pipeline Value',
+        value: 120000,
+        subttitle: 'Current Quarter'
+            }
+    cases = {
+        title: 'Open Cases',
+        value: 32,
+        subttitle: 'Pending Resolution'
+    }
+}
