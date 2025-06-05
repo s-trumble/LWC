5c242145e1aa7d6487b29e6e7b65eeb0144bda15
Steven Trumble <strumble91@gmail.com>
2025-06-05 17:51:07 +1000

    live session work

--- force-app/main/default/lwc/week6live/week6live.js
+++ force-app/main/default/lwc/week6live/week6live.js
@@ -1,6 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Week6live extends LightningElement {
+    
+    @api recordId;
+    @api objectApiName;
     showSpinner = true;
 
     people = [
@@ -8,4 +11,12 @@ export default class Week6live extends LightningElement {
         {id: 2, name: 'Bob', age: 25},
         {id: 3, name: 'Chris', age: 10}
     ]
+
+    
+    columns = [
+        {label: 'Name', fieldName: 'name', type: 'text'},
+        {label: 'Age', fieldName: 'age', type: 'number'}
+    ]
+
+    fields = ['Name', 'AnnualRevenue'];
 }
