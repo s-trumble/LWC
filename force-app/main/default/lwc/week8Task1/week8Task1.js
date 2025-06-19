5d3c8da4f671c2b8a2479e386d3ef521133092ab
Steven Trumble <strumble91@gmail.com>
2025-06-19 19:46:50 +1000

    all logic, minimal styling

--- force-app/main/default/lwc/week8Task1/week8Task1.js
+++ force-app/main/default/lwc/week8Task1/week8Task1.js
@@ -1,3 +1,45 @@
 import { LightningElement } from 'lwc';
+import getNewLeads from '@salesforce/apex/Week8Task1Controller.getNewLeads';
+import getClosedDeals from '@salesforce/apex/Week8Task1Controller.getClosedDeals';
+import getPipelineValue from '@salesforce/apex/Week8Task1Controller.getPipelineValue';
+import getOpenCases from '@salesforce/apex/Week8Task1Controller.getOpenCases';
 
-export default class Week8Task1 extends LightningElement {}
+export default class Week8Task1 extends LightningElement {
+
+    newLeads = 0;
+    closedDeals = 0;
+    pipelineValue = 0.0;
+    openCases = 0;
+
+    connectedCallback() {
+        getNewLeads()
+            .then(result => {
+                this.newLeads = result; 
+            })
+            .catch(error => {
+                console.log(`getNewLeads error: ${error}`)
+            });
+        getClosedDeals()
+            .then(result => {
+                this.closedDeals = result;
+            })
+            .catch(error => {
+                console.log(`getClosedDeals error: ${error}`)
+            });
+        getPipelineValue()
+            .then(result => {
+                this.pipelineValue = result;
+            })
+            .catch(error => {
+                console.log(`getPipelineValue error: ${error}`)
+            });
+        getOpenCases()
+            .then(result => {
+                this.openCases = result;
+            })
+            .catch(error => {
+                console.log(`getOpenCases error: ${error}`)
+            });
+
+    }
+}
