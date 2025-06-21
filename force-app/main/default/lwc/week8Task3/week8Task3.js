bea75fb2522ff86fd0bbe04c7c60d7b0155417e8
Steven Trumble <strumble91@gmail.com>
2025-06-21 17:00:13 +1000

    dynamic name input

--- force-app/main/default/lwc/week8Task3/week8Task3.js
+++ force-app/main/default/lwc/week8Task3/week8Task3.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class Week8Task3 extends LightningElement {}
+export default class Week8Task3 extends LightningElement {
+    name = '';
+
+    handleChange(event){
+        console.log('User\'s name: ', event);
+        this.name = event.detail.value;
+    }
+}
