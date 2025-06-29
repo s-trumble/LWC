786e501517eca5d745f6b3c808d084515125437e
Steven Trumble <strumble91@gmail.com>
2025-06-29 19:03:17 +1000

    add case comments and auto add to table, clear form

--- force-app/main/default/lwc/caseCommentTable/caseCommentTable.js
+++ force-app/main/default/lwc/caseCommentTable/caseCommentTable.js
@@ -1,3 +1,47 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import getCaseComments from '@salesforce/apex/caseCommentController.getCaseComments';
 
-export default class CaseCommentTable extends LightningElement {}
+export default class CaseCommentTable extends LightningElement {
+    @api recordId;
+
+    caseComments = [];
+
+    columns = [
+        {label: 'Comment', fieldName: 'Comment__c', type: 'text'},
+        {label: 'Category', fieldName: 'Category__c', type: 'text'},
+        {label: 'Visibility', fieldName: 'Visibility__c', type: 'text'},
+        {label: 'Created Date', fieldName: 'CreatedDate', type: 'datetime'},
+        {label:'Created By', fieldName: 'createdByName', type: 'text'}
+        ];
+    
+    connectedCallback(){
+        console.log('recordId: ', this.recordId);
+
+        getCaseComments({caseId: this.recordId})
+            .then(result => {
+                this.caseComments = result.map(row => ({
+                    ...row,
+                    createdByName: row.CreatedBy.Name
+                }));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    handleNewCaseComment(event) {
+        setTimeout(() => {
+            getCaseComments({caseId: this.recordId})
+            .then(result => {
+                this.caseComments = result.map(row => ({
+                    ...row,
+                    createdByName: row.CreatedBy.Name
+                }));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        },500);
+            
+    }
+}
