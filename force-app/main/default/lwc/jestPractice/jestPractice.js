17ee7d223693115d3f945edff4ac6e83149b7124
Steven Trumble <strumble91@gmail.com>
2025-07-10 19:25:27 +1000

    jest practice

--- force-app/main/default/lwc/jestPractice/jestPractice.js
+++ force-app/main/default/lwc/jestPractice/jestPractice.js
@@ -2,4 +2,8 @@ import { LightningElement } from 'lwc';
 
 export default class JestPractice extends LightningElement {
     message = 'Hello, Jest!';
+
+    handleClick() {
+        this.message = 'Button clicked';
+    }
 }
